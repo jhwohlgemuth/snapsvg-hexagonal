b15428d046fd0abf5583f0a1a929e0fa92b18498
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2017-08-01 20:43:16 -0500

    Added tests for hexagon default values for x and y

--- test/hexagon.test.js
+++ test/hexagon.test.js
@@ -6,8 +6,19 @@ const Hexagon = require('../lib/hexagon');
 Object.assign(Hexagon.prototype, mockHexagon);
 
 describe('Hexagon', function() {
+    let hexagon;
+    afterEach(() => {
+        hexagon.addClass.mockClear();
+        hexagon.polygon.mockClear();
+    });
+    it('can be created with default x and y', () => {
+        hexagon = new Hexagon(100, 0, 0);
+        expect(hexagon.addClass.mock.calls).toMatchSnapshot();
+        expect(hexagon.polygon.mock.calls).toMatchSnapshot();
+        expect(hexagon).toEqual({});
+    });
     it('can be created with correct points', () => {
-        let hexagon = new Hexagon(100, 10, 10);
+        hexagon = new Hexagon(100, 10, 10);
         expect(hexagon.addClass.mock.calls).toMatchSnapshot();
         expect(hexagon.polygon.mock.calls).toMatchSnapshot();
         expect(hexagon).toEqual({});
